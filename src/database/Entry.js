e3d21cb47fa938e6fd4bfd9aa1fed98d0a991a31
Preston Davies <pdavies@Prestons-Air-M2.fios-router.home>
2022-12-15 15:48:47 -0500

    Data creation via POST

--- src/database/Entry.js
+++ src/database/Entry.js
@@ -1,7 +1,19 @@
 const DB = require("./db.json");
+const { saveToDatabase } = require("./utils");
 
 const getAllPokemon = () => {
   return DB.pokemon;
 };
 
-module.exports = { getAllPokemon };
+const createNewEntry = (newEntry) => {
+  const isAlreadyAdded =
+    DB.pokemon.findIndex((pokemon) => pokemon.name === newEntry.name) > -1;
+  if (isAlreadyAdded) {
+    return;
+  }
+  DB.pokemon.push(newEntry);
+  saveToDatabase(DB);
+  return newEntry;
+};
+
+module.exports = { getAllPokemon, createNewEntry };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const v1PokedexRouter = require("./v1/routes/pokedexRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
 app.use("/api/v1/pokedex", v1PokedexRouter);
 
 app.listen(PORT, () => {
