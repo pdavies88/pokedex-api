78261f09b211b71eb4bed633d4ac88b57ea53732
Preston Davies <pdavies@Prestons-Air-M2.fios-router.home>
2022-12-18 22:18:45 -0500

    Error Handling

--- src/controllers/pokedexController.js
+++ src/controllers/pokedexController.js
@@ -1,18 +1,47 @@
 const pokedexService = require("../services/pokedexService");
 
 const getAllEntries = (req, res) => {
-  const allEntries = pokedexService.getAllEntries();
-  res.send({ status: "OK", data: allEntries });
+  try {
+    const allEntries = pokedexService.getAllEntries();
+    res.send({ status: "OK", data: allEntries });
+  } catch (error) {
+    res
+      .status(error?.status || 500)
+      .send({ status: "FAILED", data: { error: error?.message || error } });
+  }
 };
 
 const getOneEntry = (req, res) => {
-  const entry = pokedexService.getOneEntry();
-  res.send("Get an existing entry");
+  const {
+    params: { entryId },
+  } = req;
+  if (!entryId) {
+    res.status(400).send({
+      status: "FAILED",
+      data: { error: "Parameter ':entryId' can not be empty" },
+    });
+  }
+  try {
+    const entry = pokedexService.getOneEntry(entryId);
+    res.send({ status: "OK", data: entry });
+  } catch (error) {
+    res
+      .status(error?.status || 500)
+      .send({ status: "FAILED", data: { error: error?.message || error } });
+  }
 };
 
 const createNewEntry = (req, res) => {
   const { body } = req;
   if (!body.name || !body.type || !body.moves) {
+    // Tell users about why the create request failed
+    res.status(400).send({
+      status: "FAILED",
+      data: {
+        error:
+          "One of the following keys is missing or is empty in the request body: 'name', 'type', 'moves'",
+      },
+    });
     return;
   }
 
@@ -21,19 +50,55 @@ const createNewEntry = (req, res) => {
     type: body.type,
     moves: body.moves,
   };
-
-  const createdEntry = pokedexService.createNewEntry(newEntry);
-  res.status(201).send({ status: "OK", data: createdEntry });
+  try {
+    const createdEntry = pokedexService.createNewEntry(newEntry);
+    res.status(201).send({ status: "OK", data: createdEntry });
+  } catch (error) {
+    res
+      .status(error?.status || 500)
+      .send({ status: "FAILED", data: { error: error?.message || error } });
+  }
 };
 
 const updateOneEntry = (req, res) => {
-  const updatedEntry = pokedexService.updateOneEntry();
-  res.send("Update an existing entry");
+  const {
+    body,
+    params: { entryId },
+  } = req;
+  if (!entryId) {
+    res.status(400).send({
+      status: "FAILED",
+      data: { error: "Parameter ':entryId' can not be empty" },
+    });
+  }
+  try {
+    const updatedEntry = pokedexService.updateOneEntry(entryId, body);
+    res.send({ status: "OK", data: updatedEntry });
+  } catch (error) {
+    res
+      .status(error?.status || 500)
+      .send({ status: "FAILED", data: { error: error?.message || error } });
+  }
 };
 
 const deleteOneEntry = (req, res) => {
-  pokedexService.deleteOneEntry();
-  res.send("Delete an existing entry");
+  const {
+    params: { entryId },
+  } = req;
+  if (!entryId) {
+    res.status(400).send({
+      status: "FAILED",
+      data: { error: "Parameter ':entryId' can not be empty" },
+    });
+  }
+  try {
+    pokedexService.deleteOneEntry(entryId);
+    res.status(204).send({ status: "OK" });
+  } catch (error) {
+    res
+      .status(error?.status || 500)
+      .send({ status: "FAILED", data: { error: error?.message || error } });
+  }
 };
 
 module.exports = {
--- src/database/Entry.js
+++ src/database/Entry.js
@@ -2,18 +2,93 @@ const DB = require("./db.json");
 const { saveToDatabase } = require("./utils");
 
 const getAllPokemon = () => {
-  return DB.pokemon;
+  try {
+    return DB.pokemon;
+  } catch (error) {
+    throw { status: 500, message: error };
+  }
+};
+
+const getOnePokemon = (entryId) => {
+  try {
+    const pokemon = DB.pokemon.find((pokemon) => pokemon.id === entryId);
+    if (!pokemon) {
+      throw {
+        status: 400,
+        message: `Can't find pokemon with the id '${entryId}'`,
+      };
+    }
+    return pokemon;
+  } catch (error) {
+    throw { status: error?.status || 500, message: error?.message || error };
+  }
 };
 
 const createNewEntry = (newEntry) => {
-  const isAlreadyAdded =
-    DB.pokemon.findIndex((pokemon) => pokemon.name === newEntry.name) > -1;
-  if (isAlreadyAdded) {
-    return;
+  try {
+    const isAlreadyAdded =
+      DB.pokemon.findIndex((pokemon) => pokemon.name === newEntry.name) > -1;
+    if (isAlreadyAdded) {
+      throw {
+        status: 400,
+        message: `Pokemon with the name '${newEntry.name}' already exists`,
+      };
+    }
+
+    DB.pokemon.push(newEntry);
+    saveToDatabase(DB);
+    return newEntry;
+  } catch (error) {
+    throw { status: 500, message: error?.message || error };
   }
-  DB.pokemon.push(newEntry);
-  saveToDatabase(DB);
-  return newEntry;
 };
 
-module.exports = { getAllPokemon, createNewEntry };
+const updateOnePokemon = (entryId, changes) => {
+  try {
+    const indexForUpdate = DB.pokemon.findIndex(
+      (pokemon) => pokemon.id === entryId
+    );
+    if (indexForUpdate === -1) {
+      throw {
+        status: 400,
+        message: `Can't find pokemon with the id '${entryId}'`,
+      };
+    }
+    const updatedEntry = {
+      ...DB.pokemon[indexForUpdate],
+      ...changes,
+      updatedAt: new Date().toLocaleString("en-US", { timeZone: "UTC" }),
+    };
+    DB.pokemon[indexForUpdate] = updatedEntry;
+    saveToDatabase(DB);
+    return updatedEntry;
+  } catch (error) {
+    throw { status: error?.status || 500, message: error?.message || error };
+  }
+};
+
+const deleteOnePokemon = (entryId) => {
+  try {
+    const indexForDeletion = DB.pokemon.findIndex(
+      (pokemon) => pokemon.id === entryId
+    );
+    if (indexForDeletion === -1) {
+      throw {
+        status: 400,
+        message: `Can't find pokemon with the id '${entryId}'`,
+      };
+    }
+    DB.pokemon.splice(indexForDeletion, 1);
+    saveToDatabase(DB);
+  } catch (error) {
+    throw { status: error?.status || 500, message: error?.message || error };
+  }
+};
+
+module.exports = {
+  getAllPokemon,
+  getOnePokemon,
+  createNewEntry,
+  updateOnePokemon,
+  deleteOnePokemon,
+};
